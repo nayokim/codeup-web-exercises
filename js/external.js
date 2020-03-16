99a4090320b94aacb67c7df9f4b70d83f00f5798
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-03-16 16:55:57 -0500

    javascript intro

---
+++ js/external.js
@@ -0,0 +1,25 @@
+"use strict"
+
+console.log("Hello from external JavaScript");
+
+alert('Welcome to my website!');
+
+var userInput = prompt ('What is your favorite color?');
+alert ('Great my favorite color is ' + userInput + ' too!');
+
+
+// You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?
+
+var dailyRate = prompt('what is the daily rate?');
+
+var littleMermaid = prompt('how many days will you be renting little mermaid?');
+var brotherBear = prompt('how many days will you be renting brother bear?');
+var hercules= prompt('how many days will you be renting hercules?');
+
+var totalCost=(dailyRate * (littleMermaid + brotherBear + hercules));
+
+var promptedCost = alert( 'your total cost will be: ' + totalCost);
+
+//     Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+// A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
+//     A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
---
+++ js/javascript-with-html-lec.js
@@ -0,0 +1,2 @@
+console.log('hello from an external js file')
+
