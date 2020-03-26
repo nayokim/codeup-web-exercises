5d0de8c112d7c32095e79cf7259d32c67e9aa6f0
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-03-26 14:12:34 -0500

    postwalkthrough

--- js/planets-string.js
+++ js/planets-string.js
@@ -29,34 +29,54 @@
      */
 
     var planetsBreak = planetsArray.join('<br>');
+    document.write(planetsBreak); /// will render code as html
     console.log(planetsBreak);
     //in html, it will break a break after each word so that each word is on a new line.
-    //
+
+
     var planetsList = planetsString.split('|');
     planetsList.unshift('<ul>');
     planetsList.push('</ul>');
     planetsList.splice(1,0,'<li>');
-    planetsList.splice(3,0,'</li><br>');
+    planetsList.splice(3,0,'</li>');
     planetsList.splice(4,0,'<li>');
-    planetsList.splice(6,0,'</li><br>');
+    planetsList.splice(6,0,'</li>');
     planetsList.splice(7,0,'<li>');
-    planetsList.splice(9,0,'</li><br>');
+    planetsList.splice(9,0,'</li>');
     planetsList.splice(10,0,'<li>');
-    planetsList.splice(12,0,'</li><br>');
+    planetsList.splice(12,0,'</li>');
     planetsList.splice(13,0,'<li>');
-    planetsList.splice(15,0,'</li><br>');
+    planetsList.splice(15,0,'</li>');
     planetsList.splice(16,0,'<li>');
-    planetsList.splice(18,0,'</li><br>');
+    planetsList.splice(18,0,'</li>');
     planetsList.splice(19,0,'<li>');
-    planetsList.splice(21,0,'</li><br>');
+    planetsList.splice(21,0,'</li>');
     planetsList.splice(22,0,'<li>');
-    planetsList.splice(24,0,'</li><br>');
+    planetsList.splice(24,0,'</li>');
 
 
     console.log(planetsList);
 
-    var planets= planetsList.join('');
-    console.log(planets);
+    var planetsFinishedList= planetsList.join('');
 
+
+    console.log(planetsFinishedList);
+    document.write(planetsFinishedList);
+    //walkthrough bonus
+
+    // var planetsUL ='<ul><li>';
+    // planetsUL += planetsArray.join('</li><li>');
+    // planetsUL += "</li></ul>";
+    // document.write(planetsUL);
+
+    //for-each loop
+var planetsUL = "<ul>"; // how to start
+planetsArray.forEach(function(planet){
+    planetsUL += '<li>' + planet + "</li>"
+});
+planetsUL += "</ul>" // how to end
+
+console.log('for each loop: ' + planetsUL);
+document.write(planetsUL);
 })();
 
