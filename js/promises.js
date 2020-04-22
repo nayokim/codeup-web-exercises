2a30bb4d4990bb6ff85f3c12c8e1d0aa6459d74c
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-04-22 12:25:42 -0500

    function of promise

--- js/promises.js
+++ js/promises.js
@@ -3,20 +3,24 @@
 console.log('this is a test');
 
 
+// let username = 'nayokim';
 
-let username = 'nayokim';
-const githubPromise =  fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': 'GITHUB_KEY'}});
+function mostRecentCommit(username) {
+    const githubPromise = fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': 'GITHUB_KEY'}});
 
-console.log(githubPromise);
+    githubPromise.then(response => response.json()
+        .then(commits => {
+            // console.log(commits)
+            console.log(commits[0].created_at);
+        })
+    );
+    githubPromise.catch(error => console.log(error));
+}
 
-githubPromise.then(response => response.json()
-    .then(commits =>{
-        // console.log(commits)
-        console.log(commits[0].created_at);
-    })
-);
+mostRecentCommit('nayokim');
 
 
-
-githubPromise.catch(error => console.log(error));
+//Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
